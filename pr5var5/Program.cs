7a65034f172d04722d2597019478590d23eefa46
ИСП-22 <-22@LAB30-05>
2024-10-31 13:30:34 +0300

    Добавьте файлы проекта.

---
+++ pr5var5/Program.cs
@@ -0,0 +1,20 @@
+﻿double calcFunc = 1; //расчетная функция
+double anltFunc = 3.14 / 2; //аналитическая функция
+int n; //счетчик
+
+//цикл
+for ( n = 1; n <= 50; n++)
+{
+    calcFunc *= (4 * Math.Pow(n, 2)) / (4 * Math.Pow(n, 2) - 1);
+}
+Console.WriteLine(calcFunc); //вывод
+
+//проверка на совпадение
+if (Math.Abs(calcFunc - anltFunc) < 1.0)
+{
+    Console.WriteLine("Расчеты верны");
+}
+else
+{
+    Console.WriteLine("Расчеты не верны");
+}
